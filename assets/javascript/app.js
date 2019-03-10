2b358d53ea3037aca20f092007a2ee23ebf839d2
Keith Hemsoth <kevhemsoth@gmail.com>
2019-03-10 11:53:16 -0400

    started pull from firebase

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,14 +1,16 @@
 
-  // Initialize Firebase
-//  var config = {
-  //  apiKey: "AIzaSyC__gsKxTcrNGzLRxTn7aNbbPMZmEXTg4A",
-    //authDomain: "train-schedule-527b3.firebaseapp.com",
-  //  databaseURL: "https://train-schedule-527b3.firebaseio.com",
-  //  projectId: "train-schedule-527b3",
-  //  storageBucket: "train-schedule-527b3.appspot.com",
-  //  messagingSenderId: "1080981391959"
-  //};
-  //firebase.initializeApp(config);
+ // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyC__gsKxTcrNGzLRxTn7aNbbPMZmEXTg4A",
+    authDomain: "train-schedule-527b3.firebaseapp.com",
+    databaseURL: "https://train-schedule-527b3.firebaseio.com",
+    projectId: "train-schedule-527b3",
+    storageBucket: "train-schedule-527b3.appspot.com",
+    messagingSenderId: "1080981391959"
+  };
+  firebase.initializeApp(config);
+
+  var database = firebase.database();
 
   // Create variables to hold form input
   // Fill variables with value from form input
@@ -32,9 +34,25 @@ $("#submitButton").on("click", function(event) {
       frequency: frequency
     }
 
+    database.ref().push(newTrain);
+
     $("#input-name").val("");
     $("#input-dest").val("");
     $("#input-first").val("");
     $("#input-freq").val("");
     console.log(newTrain);
 })
+
+database.ref().on("child_added", function(trainAdded) {
+  var trainName = trainAdded.val().name;
+  var destination = trainAdded.val().destination;
+  var firstTrain = trainAdded.val().first;
+  var frequency = trainAdded.val().frequency;
+
+  
+ // $("tbody").append("<tr>");
+ // $("tr").append("<td>" + trainName);
+
+})
+
+
